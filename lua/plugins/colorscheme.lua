906d4a28aade1d8b7a56a0d96271f29fe94c47d3
Ollie <70420879+0x4F42@users.noreply.github.com>
2026-05-15 05:22:44 +0100

    bump

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,10 +2,11 @@ return {
   { "catppuccin/nvim" },
   { "slugbyte/lackluster.nvim" },
   { "metalelf0/black-metal-theme-neovim" },
+  { "webhooked/kanso.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "immortal",
+      colorscheme = "kanso-ink",
     },
   },
 }
